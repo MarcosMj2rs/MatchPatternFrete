5c2cc4f05b265c34f14ebcfe221e0b4a17c62406
Marcos J. (MJ2RS) <marcos.mj2rs@gmail.com>
2025-06-08 23:26:57 -0300

    Add project files.

---
+++ MatchPatternFrete/Program.cs
@@ -0,0 +1,21 @@
+﻿using MatchPatternFrete.Rules;
+
+Console.WriteLine("\nPattern Matching com is e switch\n");
+
+Console.ReadKey();
+
+//Definfindo algumas entregas...
+var entregas = new EntregaRule.Entrega[]
+{
+	new EntregaRule.RetiraNaloja(),
+	new EntregaRule.EntregaExpressa(15),
+	new EntregaRule.EntregaExpressa(35),
+	new EntregaRule.EntregaAgendada(DateTime.Today.AddDays(1)), // vai variar, dias da semana
+	new EntregaRule.EntregaAgendada(new DateTime(2025, 8, 10)) // omingo
+};
+
+foreach (var entrega in entregas)
+{
+	var valor = EntregaRule.CalcularFrete(entrega);
+	Console.WriteLine($"Entrega: {entrega,-70} -> Frete: {valor:C}");
+}
---
+++ MatchPatternFrete/Rules/EntregaRule.cs
@@ -0,0 +1,29 @@
+﻿namespace MatchPatternFrete.Rules
+{
+	public static class EntregaRule
+	{
+		public abstract record Entrega;
+
+		public record RetiraNaloja() : Entrega;
+
+		public record EntregaExpressa(double distanciaKm) : Entrega;
+
+		public record EntregaAgendada(DateTime dataAgendada) : Entrega
+		{
+			public override string ToString() => $"Entrega Agendada (Data: ) {dataAgendada:dddd, dd MMM, yyyy})";
+		}
+
+		public static decimal CalcularFrete(Entrega entrega)
+		{
+			return entrega switch
+			{
+				RetiraNaloja => 0m,
+				EntregaExpressa(var km) when km < 20 => 15m,
+				EntregaExpressa(var km) => 25m + (decimal)(km - 20) * 1.5m,//frete escalonado
+				EntregaAgendada { dataAgendada: var data } when data.DayOfWeek == DayOfWeek.Sunday => 50m,
+				EntregaAgendada => 20m,
+				_ => throw new ArgumentException("Tipo de entrega desconhecido.")
+			};
+		}
+	}
+}
